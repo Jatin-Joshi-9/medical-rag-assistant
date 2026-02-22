05a57a9efb0a13e4d54bb93ca1a57c46db7501eb
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-23 00:42:06 +0530

    feat: use the loader and chunker to implement the basic functionality for preprocessing the data

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from loader import PDFLoader
+from chunker import TextChunker
+
+def main():
+    file_path = "../data/Good-Clinical-Practice-Guideline.pdf"
+
+    print("file_Path: ", file_path)
+
+    pdf_loader = PDFLoader(file_path)
+    documents = pdf_loader.load()
+    pdf_loader.print_statistics(documents)
+
+    chunker = TextChunker(chunk_size=1000, chunk_overlap=200)
+    chunks = chunker.split(documents)
+    chunker.print_chunk_statistics(chunks)
+
+    print("First chunk preview:\n")
+    print(chunks[0].page_content)
+
+if __name__ == "__main__":
+    main()
