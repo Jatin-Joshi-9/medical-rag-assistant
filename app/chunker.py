f56638ba0cee29173235dabe1ff9db3f759aa41e
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-23 00:41:14 +0530

    feat: add RecursiveCharacterTextSplitter funtion in text splitter in chunker

---
+++ app/chunker.py
@@ -0,0 +1,32 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from typing import List
+
+class TextChunker:
+    def __init__(self, chunk_size: int = 1000, chunk_overlap: int = 200):
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+
+        self.splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap
+        )
+
+    def split(self, documents: List[Document]) -> List[Document]:
+        chunks = self.splitter.split_documents(documents)
+        return chunks
+
+    def print_chunk_statistics(self, chunks: List[Document]) -> None:
+        total_chunks = len(chunks)
+        total_characters = 0
+
+        for chunk in chunks:
+            total_characters += len(chunk.page_content)
+
+        average_length = total_characters // total_chunks if total_chunks > 0 else 0
+
+        print("\nChunk details:\n")
+        print(f"Chunk Size: {self.chunk_size}")
+        print(f"Chunk Overlap: {self.chunk_overlap}")
+        print(f"Total Chunks Created: {total_chunks}")
+        print(f"Average Characters per Chunk: {average_length}")
